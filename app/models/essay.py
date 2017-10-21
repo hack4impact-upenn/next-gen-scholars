e79f612a27b3ac9e0d56c065079e462fdf7ebdcc
Katie Jiang <kajiang@seas.upenn.edu>
2017-10-21 16:51:31 -0400

    Add link to essay model

--- app/models/essay.py
+++ app/models/essay.py
@@ -5,4 +5,5 @@ class Essay(db.Model):
 	__tablename__ = "essays"
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String, index=True)
-	student_profile_id = db.Column(db.Integer, db.ForeignKey('student_profile.id'), nullable=False)
+	student_profile_id = db.Column(db.Integer, db.ForeignKey('student_profile.id'), nullable=False, index=True)
+	link = db.Column(db.String, index=True)
