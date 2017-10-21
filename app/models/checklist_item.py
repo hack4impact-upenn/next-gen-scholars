2cecf099151602a9c02011e420d5f70dcc7b0a2e
stevenbursztyn <stevenbursztyn@users.noreply.github.com>
2017-10-21 16:30:36 -0400

    Spacing issues.

--- app/models/checklist_item.py
+++ app/models/checklist_item.py
@@ -1,7 +1,7 @@
 from .. import db
 
 class ChecklistItem(db.Model):
-	__tablename__ = "checklist_items"
+    __tablename__ = "checklist_items"
     id = db.Column(db.Integer, primary_key=True)
     student_profile_id = db.Column(db.Integer, db.ForeignKey('student_profiles.id'), nullable=False)
     deadline = db.Column(db.Date, index=True) 
