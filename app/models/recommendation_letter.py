c74eaa2fbc205f0758bd1f24f713b77de1f106f8
stevenbursztyn <stevenbursztyn@users.noreply.github.com>
2017-10-21 16:39:15 -0400

    Waiting -> Pending.

--- app/models/recommendation_letter.py
+++ app/models/recommendation_letter.py
@@ -9,5 +9,5 @@ class RecommendationLetter(db.Model):
 	name = db.Column(db.String, index=True)
 	# category of reference, e.g. '12th grade Calculus teacher', '11th grade biology teacher'
 	category = db.Column(db.String, index=True)
-	# statuses include 'Submitted', 'Waiting', 'Incomplete'
+	# statuses include 'Submitted', 'Pending', 'Incomplete'
 	status = db.Column(db.String, index=True)
