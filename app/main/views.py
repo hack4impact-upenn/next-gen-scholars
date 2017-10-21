93c9b9f344dcca7402800f207213562a78597cad
Hana Pearlman <pearlmanhana@gmail.com>
2017-10-21 17:24:52 -0400

    move cal, res, scatt to main/

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask_login import login_required
 from ..models import EditableHTML
 from . import main
 
@@ -13,3 +14,20 @@ def about():
     editable_html_obj = EditableHTML.get_editable_html('about')
     return render_template('main/about.html',
                            editable_html_obj=editable_html_obj)
+
+@main.route('/resources')
+@login_required
+def resources():
+    return render_template('main/resources.html')
+
+
+@main.route('/calendar', methods=['GET', 'POST'])
+@login_required
+def calendar():
+    return render_template('main/calendar.html')
+
+
+@main.route('/scattergram', methods=['GET', 'POST'])
+@login_required
+def scattergram():
+    return render_template('main/scattergram.html')
