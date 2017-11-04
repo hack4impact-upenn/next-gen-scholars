68fd4e45ebd26856a85609a5d01937371ead0a88
Katie Jiang <kajiang@seas.upenn.edu>
2017-11-03 22:53:10 -0400

    Change name to Next Generation Scholars

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ if os.path.exists('config.env'):
 
 
 class Config:
-    APP_NAME = 'Flask-Base'
+    APP_NAME = 'Next Generation Scholars'
     if os.environ.get('SECRET_KEY'):
         SECRET_KEY = os.environ.get('SECRET_KEY')
     else:
