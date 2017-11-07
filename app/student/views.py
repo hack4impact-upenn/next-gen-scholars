be049016769890b4c20370e2ff7efe4fdb276276
Katie Jiang <kajiang@seas.upenn.edu>
2017-11-07 17:06:19 -0500

    Finished styling student profile page

--- app/student/views.py
+++ app/student/views.py
@@ -2,6 +2,7 @@ from flask import abort, flash, redirect, render_template, url_for, request
 from flask_login import current_user, login_required
 from . import student
 
+
 @student.route('/checklist')
 @login_required
 def checklist():
@@ -12,4 +13,14 @@ def checklist():
 @student.route('/profile')
 @login_required
 def view_user_profile():
-	return render_template('student/student_profile.html', user = current_user)
+    sat = '––'
+    act = '––'
+    student_profile = current_user.student_profile
+    if student_profile is not None:
+        test_scores = student_profile.test_scores
+        for t in test_scores:
+            if t.name == 'SAT':
+                sat = t.score
+            if t.name == 'ACT':
+                act = t.score
+    return render_template('student/student_profile.html', user=current_user, sat=sat, act=act)
