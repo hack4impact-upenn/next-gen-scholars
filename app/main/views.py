a5df0a7033d957a2a17d3ff96708e33d2d4f5e53
Melissa Gu <melissa.gu@richmond.edu>
2018-04-01 12:46:02 -0400

    fixed gap issue on homepage, added conditionals for flash message for home page

--- app/main/views.py
+++ app/main/views.py
@@ -8,8 +8,8 @@ from . import main
 def index():
     editable_html_obj = EditableHTML.get_editable_html('index')
     return render_template(
-        'main/index.html', editable_html_obj=editable_html_obj)
-    return render_template('main/index.html')
+        'main/index.html', editable_html_obj=editable_html_obj, url='/')
+    return render_template('main/index.html', url='/')
 
 
 @main.route('/about')
