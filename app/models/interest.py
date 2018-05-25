6137a76bbf69fba9cb941667a7e2db5be419081d
Melissa Gu <melissa.gu@richmond.edu>
2018-05-24 23:09:58 -0400

    fixed db insert type into college interest student profile section

--- app/models/interest.py
+++ app/models/interest.py
@@ -3,7 +3,7 @@ from .. import db
 class Interest(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	lvl = db.Column(db.String(10), index=True)
-	name = db.Column(db.Integer, index=True)
+	name = db.Column(db.String, index=True)
 
 	def __repr__(self):
 		return '<Interest: {}>'.format(self.lvl)
