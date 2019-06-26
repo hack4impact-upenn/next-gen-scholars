1c2c72ed42890f7fedb7e1262a57c1a4cf7ff862
Rebecca Xun <rebeccaxun@gmail.com>
2019-06-26 15:28:05 -0700

    category became multi picker

--- app/student/views.py
+++ app/student/views.py
@@ -577,7 +577,11 @@ def delete_college(item_id, student_profile_id):
 def scholarships():
     """View all scholarships"""
     scholarships = Scholarship.query.all()
-    return render_template('student/scholarships.html', scholarships=scholarships)
+    category_list = ['All', "African-American","Agriculture","Arts-related","Asian","Asian Pacific American","Community Service",
+            "Construction Related Fields","Disabled","Engineering","Environmental Interest","Female","Filipino","First Generation College Student",
+            "Queer","General","Latinx","Immigrant/AB540/DACA","Interest in Journalism","Japanese","Jewish","Indigenous","Science/Engineering",
+            "Student-Athlete","Teaching","Women in Math/Engineering"]
+    return render_template('student/scholarships.html', scholarships=scholarships, category_list=category_list)
 
 
 
